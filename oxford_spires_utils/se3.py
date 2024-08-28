ba9b36869d0b3371f9a7b3483cf237d2b31fea75
Yifu Tao <ethantao96@gmail.com>
2024-08-28 18:41:55 +0100

    refactor: replace ros tf lib with scipy for quaternion conversion no need to keep the massive transformations.py just for this

--- oxford_spires_utils/se3.py
+++ oxford_spires_utils/se3.py
@@ -1,16 +1,15 @@
 import numpy as np
-
-from oxford_spires_utils.transformations import quaternion_from_matrix, quaternion_matrix
+from scipy.spatial.transform import Rotation
 
 
 def se3_matrix_to_xyz_quat_xyzw(se3_matrix):
     xyz = se3_matrix[:3, 3]
-    quat_xyzw = quaternion_from_matrix(se3_matrix[:3, :3])
+    quat_xyzw = Rotation.from_matrix(se3_matrix[:3, :3]).as_quat()
     return xyz, quat_xyzw
 
 
 def xyz_quat_xyzw_to_se3_matrix(xyz, quat_xyzw):
     se3_matrix = np.eye(4)
     se3_matrix[:3, 3] = xyz
-    se3_matrix[:3, :3] = quaternion_matrix(quat_xyzw)[:3, :3]
+    se3_matrix[:3, :3] = Rotation.from_quat(quat_xyzw).as_matrix()
     return se3_matrix
