9f7d6eb671bffddf292a836ab3ca3473ea2a656d
Yifu Tao <ethantao96@gmail.com>
2024-08-29 14:31:15 +0100

    refactor: wrap merge script into a function

--- scripts/merge_downsample_vilens_slam_clouds.py
+++ scripts/merge_downsample_vilens_slam_clouds.py
@@ -5,23 +5,23 @@ from tqdm import tqdm
 
 from oxford_spires_utils.io import read_pcd_with_viewpoint
 
-if __name__ == "__main__":
-    cloud_folder = "/home/yifu/data/nerf_data_pipeline/2024-03-18-chch_4/raw/individual_clouds"
-    downsample_resolution = 0.05
-    final_cloud_path = Path(cloud_folder).parent / f"merged_{downsample_resolution}m.ply"
 
-    # cloud_paths = list(Path(cloud_folder).rglob("*.ply"))
-    # cloud can be either ply or pcd
-
-    pcd_cloud_paths = list(Path(cloud_folder).rglob("*.pcd"))
-    ply_cloud_paths = list(Path(cloud_folder).rglob("*.ply"))
-    cloud_paths = pcd_cloud_paths + ply_cloud_paths
-
+def merge_downsample_vilens_slam_clouds(vilens_slam_clouds_folder, output_cloud_path=None, downsample_voxel_size=0.05):
+    cloud_paths = list(Path(vilens_slam_clouds_folder).rglob("*.pcd"))
+    print("Merging clouds ...")
     final_cloud = o3d.geometry.PointCloud()
     for cloud_path in tqdm(cloud_paths):
         cloud = read_pcd_with_viewpoint(str(cloud_path))
         final_cloud += cloud
-    print(f"Downsampling to {downsample_resolution}m ...")
-    final_cloud = final_cloud.voxel_down_sample(voxel_size=downsample_resolution)
-    print(f"Saving merged cloud to {final_cloud_path} ...")
-    o3d.io.write_point_cloud(str(final_cloud_path), final_cloud)
+    print(f"Downsampling to {downsample_voxel_size}m ...")
+
+    if output_cloud_path is None:
+        output_cloud_path = Path(vilens_slam_clouds_folder).parent / f"merged_{downsample_voxel_size}m.ply"
+    final_cloud = final_cloud.voxel_down_sample(voxel_size=downsample_voxel_size)
+    print(f"Saving merged cloud to {output_cloud_path} ...")
+    o3d.io.write_point_cloud(str(output_cloud_path), final_cloud)
+
+
+if __name__ == "__main__":
+    cloud_folder = "/home/yifu/data/nerf_data_pipeline/2024-03-18-chch_4/raw/individual_clouds"
+    merge_downsample_vilens_slam_clouds(cloud_folder)
