38e0b284394632b450758cc317687340a1152ca7
Yifu Tao <ethantao96@gmail.com>
2024-09-07 13:55:50 +0100

    refactor: move e57 to pcd into python module

--- oxford_spires_utils/io.py
+++ oxford_spires_utils/io.py
@@ -1,5 +1,7 @@
 import numpy as np
 import open3d as o3d
+import pye57
+from scipy.spatial.transform import Rotation
 
 from oxford_spires_utils.se3 import xyz_quat_xyzw_to_se3_matrix
 
@@ -48,3 +50,44 @@ def modify_pcd_viewpoint(file_path: str, new_file_path: str, new_viewpoint_xyz_w
             file.write(f"{line}\n".encode("utf-8"))
         file.write(b"DATA binary\n")
         file.write(binary_data)
+
+
+def convert_e57_to_pcd(e57_file_path, pcd_file_path):
+    # Load E57 file
+    e57_file = pye57.E57(e57_file_path)
+
+    # Get the first point cloud (assuming the E57 file contains at least one)
+    data = e57_file.read_scan(0, intensity=True, colors=True)
+
+    header = e57_file.get_header(0)
+    t_xyz = header.translation
+    quat_wxyz = header.rotation
+    quat_xyzw = [quat_wxyz[1], quat_wxyz[2], quat_wxyz[3], quat_wxyz[0]]
+    rot_matrix = Rotation.from_quat(quat_xyzw).as_matrix()
+    assert np.allclose(rot_matrix, header.rotation_matrix)
+
+    viewpoint_matrix = xyz_quat_xyzw_to_se3_matrix(t_xyz, quat_xyzw)
+    viewpoint = np.concatenate((t_xyz, quat_wxyz))
+
+    # Extract Cartesian coordinates
+    x = data["cartesianX"]
+    y = data["cartesianY"]
+    z = data["cartesianZ"]
+
+    # Create a numpy array of points
+    points_np = np.vstack((x, y, z)).T
+    points_homogeneous = np.hstack((points_np, np.ones((points_np.shape[0], 1))))
+    points_sensor_frame = (np.linalg.inv(viewpoint_matrix) @ points_homogeneous.T).T[:, :3]
+
+    colours = np.vstack((data["colorRed"], data["colorGreen"], data["colorBlue"])).T
+
+    # Create an Open3D PointCloud object
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points_sensor_frame)
+    # pcd.points = o3d.utility.Vector3dVector(points_np)
+    pcd.colors = o3d.utility.Vector3dVector(colours / 255)
+
+    # Save the point cloud as a PCD file
+    o3d.io.write_point_cloud(pcd_file_path, pcd)
+    print(f"PCD file saved to {pcd_file_path}")
+    modify_pcd_viewpoint(pcd_file_path, pcd_file_path, viewpoint)
