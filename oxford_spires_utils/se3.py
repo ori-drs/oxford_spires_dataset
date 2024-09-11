cef0ceaceafca626afe0b5c876603306dc68b03d
Yifu Tao <ethantao96@gmail.com>
2024-09-11 10:32:52 +0100

    feat: add se3 to xyz quat wxyz

--- oxford_spires_utils/se3.py
+++ oxford_spires_utils/se3.py
@@ -2,11 +2,25 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 
 
+def quat_xyzw_to_quat_wxyz(quat_xyzw):
+    if isinstance(quat_xyzw, list):
+        quat_xyzw = np.array(quat_xyzw)
+    assert is_quaternion(quat_xyzw), f"{quat_xyzw} is not a valid quaternion"
+    return quat_xyzw[[3, 0, 1, 2]]
+
+
 def se3_matrix_to_xyz_quat_xyzw(se3_matrix):
     assert is_se3_matrix(se3_matrix)[0], f"{se3_matrix} not valid, {is_se3_matrix(se3_matrix)[1]}"
     xyz = se3_matrix[:3, 3]
     quat_xyzw = Rotation.from_matrix(se3_matrix[:3, :3]).as_quat()
     return xyz, quat_xyzw
+
+
+def se3_matrix_to_xyz_quat_wxyz(se3_matrix):
+    assert is_se3_matrix(se3_matrix)[0], f"{se3_matrix} not valid, {is_se3_matrix(se3_matrix)[1]}"
+    xyz, quat_xyzw = se3_matrix_to_xyz_quat_xyzw(se3_matrix)
+    quat_wxyz = quat_xyzw_to_quat_wxyz(quat_xyzw)
+    return xyz, quat_wxyz
 
 
 def xyz_quat_xyzw_to_se3_matrix(xyz, quat_xyzw):
@@ -31,3 +45,11 @@ def is_se3_matrix(se3_matrix):
         "valid_orthogonality": valid_orthogonality,
     }
     return is_valid, debug_info
+
+
+def is_quaternion(quaternion):
+    if isinstance(quaternion, list):
+        quaternion = np.array(quaternion)
+    assert isinstance(quaternion, np.ndarray), f"{quaternion} is not a numpy array or list"
+    assert quaternion.shape == (4,), f"{quaternion} is not a 4D quaternion"
+    return np.isclose(np.linalg.norm(quaternion), 1.0)
