c6bd3765ea8d15314e0dd0f22d92a3c7de7b7bd4
Yifu Tao <ethantao96@gmail.com>
2024-09-17 23:45:21 +0100

    refactor: wrap lidar cloud eval to a function

--- oxford_spires_utils/point_cloud.py
+++ oxford_spires_utils/point_cloud.py
@@ -39,9 +39,10 @@ def merge_downsample_clouds(cloud_path_list, output_cloud_path, downsample_voxel
     final_cloud = final_cloud.voxel_down_sample(voxel_size=downsample_voxel_size)
     print(f"Saving merged cloud to {output_cloud_path} ...")
     o3d.io.write_point_cloud(str(output_cloud_path), final_cloud)
+    return final_cloud
 
 
 def merge_downsample_vilens_slam_clouds(vilens_slam_clouds_folder, downsample_voxel_size=0.05):
     cloud_paths = list(Path(vilens_slam_clouds_folder).rglob("*.pcd"))
     output_cloud_path = Path(vilens_slam_clouds_folder).parent / f"merged_{downsample_voxel_size}m.ply"
-    merge_downsample_clouds(cloud_paths, output_cloud_path, downsample_voxel_size)
+    return merge_downsample_clouds(cloud_paths, output_cloud_path, downsample_voxel_size)
