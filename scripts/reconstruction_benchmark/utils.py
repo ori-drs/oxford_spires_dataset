149d75ea621423ac154c76533e4d7eb70a66b752
Yifu Tao <ethantao96@gmail.com>
2024-09-18 16:10:02 +0100

    refactor: add tqdm in e57 conversion

--- scripts/reconstruction_benchmark/utils.py
+++ scripts/reconstruction_benchmark/utils.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 
+from tqdm import tqdm
+
 from oxford_spires_utils.point_cloud import convert_e57_to_pcd
 
 
 def convert_e57_folder_to_pcd_folder(e57_folder, pcd_folder):
     Path(pcd_folder).mkdir(parents=True, exist_ok=True)
     e57_files = list(Path(e57_folder).glob("*.e57"))
-    for e57_file in e57_files:
+    pbar = tqdm(e57_files)
+    print(f"Converting {len(e57_files)} E57 files in {e57_folder} to PCD files in {pcd_folder}")
+    for e57_file in pbar:
         pcd_file = Path(pcd_folder) / (e57_file.stem + ".pcd")
-        print(f"Converting {e57_file} to {pcd_file}")
+        pbar.set_description(f"Processing {e57_file.name}")
         convert_e57_to_pcd(e57_file, pcd_file, pcd_lib="pypcd4")
