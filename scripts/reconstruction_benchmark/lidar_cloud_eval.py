45054041a524d5432c556669a21c745af8cb4fc6
Yifu Tao <ethantao96@gmail.com>
2024-09-18 17:04:56 +0100

    fix: lidar eval

--- scripts/reconstruction_benchmark/lidar_cloud_eval.py
+++ scripts/reconstruction_benchmark/lidar_cloud_eval.py
@@ -8,7 +8,12 @@ from spires_cpp import convertOctreeToPointCloud, processPCDFolder, removeUnknow
 
 
 def evaluate_lidar_cloud(
-    project_folder, lidar_cloud_folder_path, gt_folder_path, octomap_resolution=0.1, downsample_voxel_size=0.05
+    project_folder,
+    lidar_cloud_folder_path,
+    gt_octree_path,
+    gt_cloud_path,
+    octomap_resolution=0.1,
+    downsample_voxel_size=0.05,
 ):
     input_cloud_bt_path = Path(project_folder) / "input_cloud.bt"
     processPCDFolder(str(lidar_cloud_folder_path), octomap_resolution, str(input_cloud_bt_path))
@@ -20,9 +25,9 @@ def evaluate_lidar_cloud(
     input_cloud_merged_path = Path(project_folder) / "input_cloud_merged.ply"
     _ = merge_downsample_vilens_slam_clouds(lidar_cloud_folder_path, downsample_voxel_size, input_cloud_merged_path)
     input_cloud_filtered_path = Path(project_folder) / "input_cloud_merged_filtered.ply"
-    removeUnknownPoints(str(input_cloud_merged_path), str(gt_cloud_bt_path), str(input_cloud_filtered_path))
+    removeUnknownPoints(str(input_cloud_merged_path), str(gt_octree_path), str(input_cloud_filtered_path))
     input_cloud_np = np.asarray(o3d.io.read_point_cloud(str(input_cloud_filtered_path)).points)
-    gt_cloud_np = np.asarray(self.gt_cloud_merged.points)
+    gt_cloud_np = np.asarray(o3d.io.read_point_cloud(str(gt_cloud_path)).points)
 
     print(get_recon_metrics(input_cloud_np, gt_cloud_np))
     save_error_cloud(input_cloud_np, gt_cloud_np, str(Path(project_folder) / "input_error.ply"))
