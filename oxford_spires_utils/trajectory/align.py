ed59315f382c04fddf42056539b3118afb18f435
Yifu Tao <ethantao96@gmail.com>
2024-09-20 15:43:01 +0100

    refactor: rename output_dir for evo alignment

--- oxford_spires_utils/trajectory/align.py
+++ oxford_spires_utils/trajectory/align.py
@@ -4,7 +4,7 @@ import numpy as np
 from evo.core import sync
 
 
-def align(vilens_traj, colmap_traj, colmap_dir):
+def align(vilens_traj, colmap_traj, output_dir):
     vilens_traj_sync, colmap_traj_sync = sync.associate_trajectories(vilens_traj, colmap_traj, max_diff=0.05)
     print(f"Trajectory pair length: {len(vilens_traj_sync.poses_se3)}")
     r_a, t_a, align_s = colmap_traj_sync.align(vilens_traj_sync, correct_scale=True)
@@ -14,7 +14,7 @@ def align(vilens_traj, colmap_traj, colmap_dir):
         "scale": align_s,
     }
     print("Save align data to evo_align_results.json")
-    with open(colmap_dir / "evo_align_results.json", "w") as f:
+    with open(output_dir / "evo_align_results.json", "w") as f:
         json.dump(align_data, f, indent=2)
     print(json.dumps(align_data, indent=2))
     T_vilens_colmap = np.eye(4)
