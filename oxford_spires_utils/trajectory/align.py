a21e21b9a54649b0e272005567abac20d079383a
Yifu Tao <ethantao96@gmail.com>
2024-09-23 00:11:31 +0100

    feat: add ape check in scale correction

--- oxford_spires_utils/trajectory/align.py
+++ oxford_spires_utils/trajectory/align.py
@@ -2,6 +2,7 @@ import json
 
 import numpy as np
 from evo.core import sync
+from evo.core.metrics import APE
 
 
 def align(vilens_traj, colmap_traj, output_dir):
@@ -13,6 +14,14 @@ def align(vilens_traj, colmap_traj, output_dir):
         "translation": t_a.tolist(),
         "scale": align_s,
     }
+    ape = APE()
+    ape.process_data((vilens_traj_sync, colmap_traj_sync))
+    ape.get_all_statistics()
+    print("APE statistics:")
+    print(ape.get_all_statistics())
+    mean_ape = ape.get_all_statistics()["mean"]
+    if mean_ape > 0.1:
+        print(f"APE mean {mean_ape} > 0.1, consider checking the alignment")
     print("Save align data to evo_align_results.json")
     with open(output_dir / "evo_align_results.json", "w") as f:
         json.dump(align_data, f, indent=2)
