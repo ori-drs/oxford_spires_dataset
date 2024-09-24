a58eafc5ec2454b5e15e42f33271496cdcd65958
mice85 <munoz.banon@gmail.com>
2024-09-24 16:16:29 +0100

    paths adapted to the docker

--- scripts/localisation_benchmark/fast_lio2.py
+++ scripts/localisation_benchmark/fast_lio2.py
@@ -2,26 +2,26 @@ import libtmux
 
 if __name__ == "__main__":
     
-    path = '/media/mice85/blackdrive/oxford_spires_dataset/data/'
+    path = '/home/oxford_spires_dataset/data/'
     sequence = '2024-03-14_blenhein_1'
     seq_path = path + sequence
     
     server = libtmux.Server()
     server.cmd('new-session', '-d', '-P', '-F#{session_id}')
     session = server.sessions[0]
-    window_1 = session.new_window(attach=False, window_name="roscore")
+    window_1 = session.new_window(attach=False, window_name="launch fast_lio2")
     window_2 = session.new_window(attach=False, window_name="rosbag") 
     pane_1 = window_1.split()
     pane_2 = window_2.split()
     
-    pane_1.send_keys('cd ~/oxford-lab/oxford_ws/')
+    pane_1.send_keys('cd /home/catkin_ws/')
     pane_1.send_keys('source devel/setup.bash')
     pane_1.send_keys('roslaunch fast_lio {}.launch'.format(sequence))
     
     pane_2.send_keys('cd {}/rosbag/'.format(seq_path))
     pane_2.send_keys('rosbag play *.bag --clock')
 
-    # Check when the rosbag is "Done."
+    # Check if the rosbag is "Done."
     while (1):
         pane_2.clear()
         cap_curr = pane_2.capture_pane()
