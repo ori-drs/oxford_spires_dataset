ea67e500c91747b88ca7dd7069d3bfd286141f46
mice85 <munoz.banon@gmail.com>
2024-09-25 16:37:20 +0100

    fast_lio now implemented and output saved

--- scripts/localisation_benchmark/fast_lio2.py
+++ scripts/localisation_benchmark/fast_lio2.py
@@ -1,4 +1,6 @@
 import libtmux
+import time
+import sys
 
 if __name__ == "__main__":
     
@@ -11,25 +13,34 @@ if __name__ == "__main__":
     session = server.sessions[0]
     window_1 = session.new_window(attach=False, window_name="launch fast_lio2")
     window_2 = session.new_window(attach=False, window_name="rosbag") 
-    pane_1 = window_1.split()
-    pane_2 = window_2.split()
+    pane_1 = window_1.panes.get()
+    pane_2 = window_2.panes.get()
     
     pane_1.send_keys('cd /home/catkin_ws/')
     pane_1.send_keys('source devel/setup.bash')
     pane_1.send_keys('roslaunch fast_lio {}.launch'.format(sequence))
     
     pane_2.send_keys('cd {}/rosbag/'.format(seq_path))
-    pane_2.send_keys('rosbag play *.bag --clock')
+    pane_2.send_keys('rosbag play *.bag --clock -s 470')
 
     # Check if the rosbag is "Done."
+    print("Fast-LIO2 launched!! - {}.launch".format(sequence))
+    print("Starting rosbag files in {}/rosbag/ ...".format(seq_path))
     while (1):
         pane_2.clear()
+        time.sleep(1)
         cap_curr = pane_2.capture_pane()
         for line in cap_curr:
             if "[RUNNING]" in line:
                 print(line)
+                sys.stdout.write("\033[F")
         if 'Done.' in cap_curr:
+            sys.stdout.write("\033[K")
+            print('Done.') 
             break
+
+    print("PROCESS FINISHED!!")
+    print("Output in: {}/output_loc/Fast-LIO2.bag".format(sequence))
 
     # TODO: Copy to output folder
 
