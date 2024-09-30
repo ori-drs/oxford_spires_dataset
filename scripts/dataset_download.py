d8af1a2c4b36d3a0762f47c92d32c3afa1ed2ded
Yifu Tao <ethantao96@gmail.com>
2024-09-30 15:57:15 +0100

    feat: use huggingface_hub package for downloa

--- scripts/dataset_download.py
+++ scripts/dataset_download.py
@@ -1,28 +1,23 @@
 import logging
 import shutil
-import subprocess
 from pathlib import Path
+
+from huggingface_hub import hf_hub_download
 
 logger = logging.getLogger(__name__)
 
-dataset_urls = {
-    "2024-03-13-roq-1": "https://huggingface.co/datasets/yifutao/OxSpires/resolve/main/2024-03-13-roq-1/",
-    "2024-03-14-blenheim-5": "https://huggingface.co/datasets/yifutao/OxSpires/resolve/main/2024-03-14-blenheim-5/",
-}
-
+repo_id = "ori-drs/oxford_spires_dataset"
+dataset_sequences = ["2024-03-13-roq-1", "2024-03-14-blenheim-5"]
 file_lists = ["images.zip", "lidar_slam.zip", "gt/individual_e57_clouds.zip", "T_gt_lidar.txt"]
-for dataset in dataset_urls:
-    output_folder = Path(__file__).parent.parent / "data" / dataset
+for sequence in dataset_sequences:
+    output_folder = Path(__file__).parent.parent / "data"
     Path(output_folder).mkdir(parents=True, exist_ok=True)
+    logger.info(f"Downloading {sequence} to {output_folder}")
     for file in file_lists:
-        full_url = dataset_urls[dataset] + file
-        save_path = (output_folder / file).parent
-        logger.info(f"Downloading {full_url} to {save_path}")
-        subprocess.run(["wget", "-P", save_path, full_url], check=False)
-
+        hf_hub_download(repo_id, repo_type="dataset", filename=file, subfolder=sequence, local_dir=str(output_folder))
     files = list(output_folder.rglob("*.zip"))
     for file in files:
         logger.info(f"Unzipping {file}")
         shutil.unpack_archive(file, file.parent)
         file.unlink()
-    logger.info(f'Complete downloading "{dataset}" to {output_folder}')
+    logger.info(f'Complete downloading "{sequence}" to {output_folder}')
