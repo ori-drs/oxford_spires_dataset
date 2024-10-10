1423cba44d7833c4312d2bb8d73d2ee08a3253c1
Yifu Tao <ethantao96@gmail.com>
2024-10-10 12:04:20 +0100

    feat: add script to get train-eval from time window

--- scripts/reconstruction_benchmark/nerf_json.py
+++ scripts/reconstruction_benchmark/nerf_json.py
@@ -1,35 +1,62 @@
 from copy import deepcopy
 from pathlib import Path
 
+from oxford_spires_utils.trajectory.file_interfaces.nerf import NeRFTrajReader
 from oxford_spires_utils.trajectory.nerf_json_handler import NeRFJsonHandler
+from oxford_spires_utils.trajectory.utils import pose_to_ply
 
 
-def merge_two_json(json1, json2):
-    assert json1.traj.keys() == json2.traj.keys()
-    new_json = deepcopy(json1)
-    new_json.traj["frames"] += json2.traj["frames"]
-    return new_json
+def split_json(json_file, start_time, end_time, new_name):
+    nerf_json_handler = NeRFJsonHandler(json_file)
+    nerf_json_handler.keep_timestamp_only(start_time, end_time)
+    save_path = Path(json_file).parent / f"{new_name}.json"
+    nerf_json_handler.save_json(save_path)
+
+    nerf_traj = NeRFTrajReader(save_path)
+    nerf_pose = nerf_traj.read_file()
+    pose_ply_file = Path(json_file).parent / f"{new_name}.ply"
+    pose_to_ply(nerf_pose, pose_ply_file)
 
 
-image_dir = "/home/docker_dev/oxford_spires_dataset/data/roq-full/images"
-json_train_file = "/home/docker_dev/oxford_spires_dataset/data/roq-full/outputs/colmap/seq_1_fountain.json"
-json_eval_file = "/home/docker_dev/oxford_spires_dataset/data/roq-full/outputs/colmap/seq_1_fountain_back.json"
-new_image_dir = Path(image_dir).parent.resolve() / "images_train_eval"
-merged_json_file = Path(json_train_file).parent / "merged.json"
+def merge_json_files(json_train_file, json_eval_file, image_dir, new_image_dir, merged_json_file):
+    json_train = NeRFJsonHandler(json_train_file)
+    json_eval = NeRFJsonHandler(json_eval_file)
+    json_train.rename_filename(
+        old_folder="images", new_folder=new_image_dir.stem, prefix="train_", base_folder=str(Path(image_dir).parent)
+    )
+    json_eval.rename_filename(
+        old_folder="images", new_folder=new_image_dir.stem, prefix="eval_", base_folder=str(Path(image_dir).parent)
+    )
+    # merge
+    assert json_train.traj.keys() == json_eval.traj.keys()
+    new_json = deepcopy(json_train)
+    new_json.traj["frames"] += json_eval.traj["frames"]
+    new_json.save_json(str(merged_json_file))
 
 
-json_train = NeRFJsonHandler(json_train_file)
-json_eval = NeRFJsonHandler(json_eval_file)
-json_train.rename_filename(
-    old_folder="images", new_folder=new_image_dir.stem, prefix="train_", base_folder=str(Path(image_dir).parent)
-)
-json_eval.rename_filename(
-    old_folder="images", new_folder=new_image_dir.stem, prefix="eval_", base_folder=str(Path(image_dir).parent)
-)
+dataset_folder = "/home/docker_dev/oxford_spires_dataset/data/roq-full"
+
+train_name = "seq_1_fountain"
+start_time = 1710338123.042936934
+end_time = 1710338186.342030327
+
+eval_name = "seq_1_fountain_back"
+start_time = 1710338353.039451086
+end_time = 1710338386.638942551
+
+dataset_folder = Path(dataset_folder).resolve()
+colmap_folder = dataset_folder / "outputs/colmap"
+json_file = colmap_folder / "transforms.json"
+
+split_json(json_file, start_time, end_time, train_name)
+split_json(json_file, start_time, end_time, eval_name)
 
 
-# merge
-new_json = merge_two_json(json_train, json_eval)
-new_json.save_json(str(merged_json_file))
+image_dir = dataset_folder / "images"
+json_train_file = colmap_folder / (train_name + ".json")
+json_eval_file = colmap_folder / (eval_name + "json")
+new_image_dir = Path(image_dir).parent / "images_train_eval"
+merged_json_file = colmap_folder / "transforms_train_eval.json"
 
+merge_json_files(json_train_file, json_eval_file, image_dir, new_image_dir, merged_json_file)
 # create json with the new train/eval prefix
