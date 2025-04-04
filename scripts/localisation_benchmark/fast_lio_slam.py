2604129f37dbe2c1ec8422e51db639079c9c4b3d
MunozBanon <miguelangel.munoz@ua.es>
2025-04-04 16:10:18 +0100

    refactor: Main file added to loc benchmark

--- scripts/localisation_benchmark/fast_lio_slam.py
+++ scripts/localisation_benchmark/fast_lio_slam.py
@@ -9,10 +9,6 @@ import numpy as np
 from evo.core.trajectory import PoseTrajectory3D
 from evo.tools import file_interface
 
-sys.path.append("/home/mice85/oxford-lab/labrobotica/algorithms/oxford_spires_dataset")
-
-from oxford_spires_utils.bash_command import run_command
-
 
 def run_fast_lio_slam (path_to_rosbag, path_to_output):
     
@@ -95,83 +91,3 @@ def convert_to_tum (path_to_output, path_to_sec):
     print("PROCESS FINISHED!!")
     print("Output in: {}/output_slam/fast_lio_tum.txt".format(path_to_sec))
     print("*********************************************************")
-
-
-def eval_fast_lio_slam (path_to_gt, path_to_output, package_dir, path_to_sec, dataset_dir):
-    
-    path_traj = path_to_sec + "/output_slam" + "/fast_lio_tum.txt"
-    output = run_command("evo_ape tum {} {} --align --t_max_diff 0.01".format(path_to_gt, path_traj), print_output=False)
-    
-    rmse = -1
-    for line in output.stdout:
-        print(line, end="")
-        if "rmse" in line:
-            numbers = re.findall('\d+\.\d+|\d+', line)
-            rmse = numbers[0]
-    
-    logging.basicConfig(filename=dataset_dir + "results.log", filemode="a", level=logging.INFO)
-    logging.info(path_to_sec)
-    logging.info("APE - RMSE result (Fast_LIO-SLAM): {}".format(rmse))
-    print("RMSE added to log: {}".format(rmse))
-
-def create_output_folder (path_to_sec):
-    path_to_output = path_to_sec + "/output_slam" + "/fastlio_raw_output/"
-    if not os.path.exists(path_to_output):
-            os.makedirs(path_to_output)
-    return path_to_output
-
-def get_sec_list (dataset_dir, flag_is_all=True):
-    if flag_is_all:
-        list_sec = os.listdir(dataset_dir)
-    else:
-        list_sec = [
-                    "2024-03-12-keble-college-02",
-                    "2024-03-12-keble-college-03",
-                    "2024-03-12-keble-college-04",
-                    "2024-03-12-keble-college-05",
-                    "2024-03-13-observatory-quarter-01",
-                    "2024-03-13-observatory-quarter-02",
-                    "2024-03-14-blenheim-palace-01",
-                    "2024-03-14-blenheim-palace-02",
-                    "2024-03-14-blenheim-palace-05",
-                    "2024-03-18-christ-church-01",
-                    "2024-03-18-christ-church-02",
-                    "2024-03-18-christ-church-03",
-                    "2024-03-20-christ-church-05",
-                    "2024-05-20-bodleian-library-02",
-                    "2024-05-20-bodleian-library-03",
-                    "2024-05-20-bodleian-library-04",
-                    "2024-05-20-bodleian-library-05"
-                    ]
-    return list_sec
-
-if __name__ == "__main__":
-
-    # -------------------------------------------------------------------------------- #
-    # TODO: get path from arg an define folders in the future class.
-    package_dir = "/home/mice85/oxford-lab/labrobotica/algorithms/oxford_spires_dataset"
-    dataset_dir = "/media/mice85/blackdrive1/oxford_spires_dataset/data/" 
-    flag_is_all = False
-    # -------------------------------------------------------------------------------- #
-
-    list_sec = get_sec_list (dataset_dir, flag_is_all)
-    
-    print('Total sequence folders: ' + str(len(list_sec)))
-    
-    # Print list of sequences
-    for sec in list_sec:
-
-        path_to_sec = dataset_dir + sec
-        path_to_rosbag = "/home/mice85/data/" + sec + "/rosbag/" #path_to_sec + "/rosbag/"
-        path_to_gt = path_to_sec + "/ground_truth_traj/gt_lidar.txt"
-        path_to_output = create_output_folder (path_to_sec)
-
-        run_fast_lio_slam (path_to_rosbag, path_to_output)
-
-        time.sleep(5)
-
-        convert_to_tum (path_to_output, path_to_sec)
-
-        eval_fast_lio_slam (path_to_gt, path_to_output, package_dir, path_to_sec, dataset_dir)
-
-        # break
---
+++ scripts/localisation_benchmark/main.py
@@ -0,0 +1,82 @@
+import argparse
+import os
+from pathlib import Path
+
+import yaml
+from fast_lio_slam import run_fast_lio_slam, convert_to_tum
+
+# TODO: loggin
+# def setup_logging():
+
+class LocalisationBenchmark:
+    def __init__(self, loc_config):
+        self.loc_config = loc_config
+        self.gt_file = "/processed/trajectory/gt-tum.txt"
+    
+    def process_fast_lio_slam (self):
+        print("run_fast_lio_slam here")
+        print("convert_to_tum here")
+    
+    def evaluation_ape_rmse (path_to_gt, path_traj, dataset_dir, method):
+        output = run_command("evo_ape tum {} {} --align --t_max_diff 0.01".format(path_to_gt, path_traj), print_output=False)
+        
+        rmse = -1
+        for line in output.stdout:
+            print(line, end="")
+            if "rmse" in line:
+                numbers = re.findall('\d+\.\d+|\d+', line)
+                rmse = numbers[0]
+        
+        # logging.basicConfig(filename=dataset_dir + "results.log", filemode="a", level=logging.INFO)
+        # logging.info(path_to_sec)
+        # logging.info("APE - RMSE result ({}): {}".format(method, rmse))
+        print("RMSE added to log: {}".format(rmse))
+
+        return rmse
+
+def create_output_folder (path_to_sec, method_folder):
+    path_to_output = path_to_sec + "/output_slam" + method_folder
+    if not os.path.exists(path_to_output):
+            os.makedirs(path_to_output)
+    return path_to_output
+
+def get_sec_list (loc_config):
+    if loc_config["flag_is_all"]:
+        list_sec = os.listdir(loc_config["dataset_folder"])
+    else:
+        list_sec = loc_config["list_sec"]
+    return list_sec
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Localisation Benchmark")
+    default_loc_config_file = Path(__file__).parent.parent.parent / "config" / "loc_benchmark.yaml"
+    parser.add_argument(
+        "--config-file",
+        type=str,
+        default=str(default_loc_config_file),
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    # setup_logging()
+    loc_config_file = get_args().config_file
+    # logger.info("Starting Reconstruction Benchmark")
+    with open(loc_config_file, "r") as f:
+        loc_config = yaml.safe_load(f)["localisation_benchmark"]
+    
+    list_sec = get_sec_list(loc_config)
+    print('Total sequence folders: ' + str(len(list_sec)))
+
+    loc_benchmark = LocalisationBenchmark(loc_config)
+
+    for sec in list_sec:
+
+        path_to_sec = loc_benchmark.loc_config["dataset_folder"] + sec
+        path_to_gt = path_to_sec + loc_benchmark.gt_file
+
+        print("SEQUENCE: " + path_to_sec)
+
+        if loc_benchmark.loc_config["run_fast_lio_slam"]:
+            print("RUNING FAST-LIO-SLAM")
+            path_to_output = create_output_folder (path_to_sec, "/fastlio_raw_output/")
+            loc_benchmark.process_fast_lio_slam
