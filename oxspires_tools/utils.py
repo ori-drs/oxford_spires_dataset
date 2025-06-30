ceb5bb2b0a89da43e796900ed33eae614b98c13a
Yifu Tao <ethantao96@gmail.com>
2025-06-30 18:14:18 +0100

    feat: add depth generation script and utils

--- oxspires_tools/utils.py
+++ oxspires_tools/utils.py
@@ -1,4 +1,7 @@
+import re
+from bisect import bisect_left
 from pathlib import Path
+from typing import List, Tuple
 
 import numpy as np
 import open3d as o3d
@@ -69,3 +72,119 @@ def get_pcd(points, colors=None):
         assert colors.max() <= 1.0 and colors.min() >= 0.0, "Colors should be in range [0, 1]"
         pcd.colors = o3d.utility.Vector3dVector(colors)
     return pcd
+
+
+def get_accumulated_pcd(current_pcd, transforms, accumulation_length=0, max_time_diff_camera_and_pose=0.0):
+    def get_T_WB_from_timestamp(timestamp, transforms, max_time_diff_camera_and_pose=0.0):
+        closest_timestamp = min(transforms.keys(), key=lambda x: abs(float(x) - float(timestamp)))
+        diff = abs(float(closest_timestamp) - float(timestamp))
+        if diff > max_time_diff_camera_and_pose:
+            # print(f"WARNING: {timestamp}'S diff from transforms is {diff} > {max_time_diff_camera_and_pose} ")
+            return None
+        if max_time_diff_camera_and_pose == 0 and timestamp != closest_timestamp:
+            print(f"Warning: closest timestamp is {closest_timestamp} but current timestamp is {timestamp}")
+            input("Press Enter to continue...")
+        timestamp = closest_timestamp
+        return transforms[timestamp]
+
+    # Get pcd directory
+    pcd_dir = current_pcd.parent
+
+    pcd_paths = sorted(list(pcd_dir.glob("*pcd")))
+    accumulated_pcd_paths = []
+    current_idx = pcd_paths.index(current_pcd)
+    for i in range(accumulation_length * 2 + 1):
+        new_idx = current_idx - accumulation_length + i
+        if new_idx < 0 or new_idx >= len(pcd_paths):
+            continue
+        accumulated_pcd_paths.append(pcd_paths[new_idx])
+
+    accumulated_pcd = o3d.geometry.PointCloud()
+    current_timestamp = current_pcd.stem[6:].replace("_", ".")
+    # remove tailing zeros
+    current_timestamp = re.sub(r"0+$", "", current_timestamp)
+    T_WB = get_T_WB_from_timestamp(current_timestamp, transforms, max_time_diff_camera_and_pose)
+    if T_WB is None:
+        print(f"current_timestamp: {current_timestamp} not found")
+        return None
+    # print(f"Current timestamp: {current_timestamp}")
+    for pcd_path in accumulated_pcd_paths:
+        pcd = o3d.io.read_point_cloud(pcd_path.as_posix())
+        that_timestamp = pcd_path.stem[6:].replace("_", ".")
+        # print(f"that timestamp: {that_timestamp}")
+        # remove tailing zeros
+        that_timestamp = re.sub(r"0+$", "", that_timestamp)
+        # Accumulate pcd if pose exists
+
+        T_WA = get_T_WB_from_timestamp(that_timestamp, transforms, max_time_diff_camera_and_pose)
+        if T_WA is None:
+            # print(f"skipping {that_timestamp} for {current_timestamp}")
+            continue
+        T_BA = np.linalg.inv(T_WB) @ T_WA
+        accumulated_pcd += pcd.transform(T_BA)
+    # axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1)
+    # o3d.visualization.draw_geometries([axis,accumulated_pcd])
+    return accumulated_pcd
+
+
+def find_closest_in_sorted(array: List, value: float):
+    """
+    Find and return closest value and difference
+    """
+    # Over or under any value in the array
+    assert isinstance(value, (float, np.float128))
+    if array[-1] < value:
+        return array[-1], value - array[-1], len(array) - 1
+    if array[0] > value:
+        return array[0], array[0] - value, 0
+
+    # Find closest value using binary search
+    idx = bisect_left(array, value)
+    diff_left = abs(value - array[idx - 1])
+    diff_right = abs(array[idx] - value)
+    if diff_left < diff_right:
+        return array[idx - 1], diff_left, idx - 1
+    else:
+        return array[idx], diff_right, idx
+
+
+def get_image_pcd_sync_pair(
+    image_dir: Path, pcd_dir: Path, image_ext: str, timestamp_threshold: float = 0.05
+) -> List[Tuple[Path, Path, float]]:
+    """
+    Find image and point cloud pairs that are synchronized based on their timestamps.
+
+    Args:
+        image_dir (Path): Directory containing the image files.
+        pcd_dir (Path): Directory containing the point cloud files.
+        image_ext (str): File extension of the image files (e.g. ".jpg").
+        timestamp_threshold (float): Maximum time difference allowed between an image and a point cloud, in seconds.
+            Defaults to 0.05.
+
+    Returns:
+        List[Tuple[Path, Path, float]]: A list of tuples, where each tuple represents an image and a point cloud that are
+            synchronized, along with their time difference in seconds.
+    """
+    # Load images and timestamps
+    image_timestamps = []
+    image_paths = {}
+    for it in sorted(list(image_dir.glob(f"*{image_ext}"))):
+        ret = re.findall(r"\d+", it.name)
+        timestamp = float(".".join(ret))
+        image_timestamps.append(timestamp)
+        image_paths[timestamp] = it
+    assert len(image_paths) > 0, "No images are found"
+    print(f"Loaded {len(image_paths)} images")
+
+    # Collect all image and lidar pair which have timestamp close enough
+    image_pcd_pairs = []
+    for it in sorted(list(Path(pcd_dir).glob("*pcd"))):
+        ret = re.findall(r"\d+", it.name)
+        timestamp = float(".".join(ret))
+        image_timestamp, diff, _ = find_closest_in_sorted(image_timestamps, timestamp)
+        # print(diff)
+        if diff < timestamp_threshold:
+            image_pcd_pairs.append((image_paths[image_timestamp], it, diff))
+
+    print(f"Found {len(image_pcd_pairs)} image-pcd pairs")
+    return image_pcd_pairs
---
+++ scripts/generate_depth.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+import shutil
+from pathlib import Path
+
+import numpy as np
+import open3d as o3d
+from tqdm.auto import tqdm
+
+from oxspires_tools.depth.main import get_depth_from_cloud
+from oxspires_tools.depth.utils import save_projection_outputs
+from oxspires_tools.trajectory.nerf_traj_reader import NeRFTrajReader
+from oxspires_tools.trajectory.pose_convention import PoseConvention
+from oxspires_tools.trajectory.vilens_slam_traj_reader import VilensSlamTrajReader
+from oxspires_tools.utils import get_accumulated_pcd, get_image_pcd_sync_pair
+
+
+def project_lidar_to_fisheye(
+    sensor,
+    project_dir: str,
+    depth_dir: str,
+    normal_dir: str,
+    camera_topics_labelled: dict,
+    image_folder_path: str,
+    image_folder_name: str,
+    depth_pose_path: str,
+    depth_pose_format: str,
+    slam_individual_clouds_new_path: str,
+    is_euclidean: bool,
+    accum_length: int,
+    max_time_diff_camera_and_pose: float,
+    image_ext: str = ".jpg",  # image extension
+    depth_factor: float = 256.0,  # depth encoding factor: (depth*depth_encode_factor).astype(np.uint16)",
+    save_overlay: bool = True,
+    save_normal_map: bool = True,
+    pose_scale_factor: float = 1.0,  # not 1 if colmap pose
+    camera_model: str = "OPENCV_FISHEYE",
+    T_cam_base_overwrite: np.ndarray = {},
+):
+    # args = get_args()
+    # Project dir
+    proj_dir = Path(project_dir).expanduser()
+
+    if is_euclidean:
+        print("Depth is euclidean: L2 distance between points and camera")
+    else:
+        print("Depth is not euclidean: z_value")
+    # Path settings
+    # pcd_dir = proj_dir / "individual_clouds"
+    # image_dir = proj_dir / "images"
+    overlay_dir = proj_dir / (depth_dir + "_overlay")
+    output_depth_dir = proj_dir / depth_dir
+    if output_depth_dir.exists():
+        shutil.rmtree(output_depth_dir)
+    if save_overlay and overlay_dir.exists():
+        shutil.rmtree(overlay_dir)
+    output_depth_dir.mkdir(parents=True)
+
+    if save_normal_map:
+        output_normal_dir = proj_dir / normal_dir
+        if output_normal_dir.exists():
+            shutil.rmtree(output_normal_dir)
+        output_normal_dir.mkdir(parents=True)
+
+    image_ext = image_ext if image_ext[0] == "." else f".{image_ext}"
+    cam_subdirs = list(camera_topics_labelled.values())
+    target_cam_names = list(camera_topics_labelled.keys())
+
+    # Loop over each camera
+    for subdir, cam_name in zip(cam_subdirs, target_cam_names):
+        print(f"Processing {cam_name} in {subdir} ...")
+        # Transformation from base to camera (clouds in individual_clouds are base coordinates)
+        # T_cam_lidar = np.linalg.inv(Ts[f"base_{cam_name}"]) @ Ts[f"base_{lidar_name}"]
+        K, D, h, w, fov_deg, _ = sensor.get_params_for_depth(cam_name, depth_pose_format, depth_pose_path)
+        T_base_cam = sensor.tf.get_transform(cam_name, "base")
+        if cam_name in T_cam_base_overwrite.keys():
+            # use the overwriten T_cam_base using T_cam_lidar_overwrite
+            T_cam_base = T_cam_base_overwrite[cam_name]
+        else:
+            # just use the default T_cam_base
+            T_cam_base = np.linalg.inv(T_base_cam)
+
+        # Setup input dir
+        target_image_subdir = image_folder_path / subdir
+        target_depth_subdir = output_depth_dir / subdir
+        target_depth_subdir.mkdir(parents=True, exist_ok=True)
+        if save_normal_map:
+            target_normal_subdir = output_normal_dir / subdir
+            target_normal_subdir.mkdir(parents=True, exist_ok=True)
+        if save_overlay:
+            target_overlay_subdir = overlay_dir / subdir
+            target_overlay_subdir.mkdir(parents=True, exist_ok=True)
+
+        # Project lidar points on images and  save as 16 bit depth
+        print(f"Target image directory: {target_image_subdir}")
+        print(f"Target depth directory: {target_depth_subdir}")
+
+        # Get image and pcd sync pairs
+        image_pcd_pairs = get_image_pcd_sync_pair(
+            target_image_subdir,
+            slam_individual_clouds_new_path,
+            image_ext,
+            max_time_diff_camera_and_pose,
+        )
+
+        # Loop for one camera
+        print(f"Fov: {fov_deg}")
+        # load all lidar poses as T_WB, so assume vilens-processed lidar in base frame
+        T_WBs = get_transforms(
+            depth_pose_path,
+            depth_pose_format,
+            pose_scale_factor,
+            T_base_cam,
+            subdir,
+            image_folder_name,
+            frame="base",
+            visualise=False,
+        )
+        for image_path, pcd_path, diff in tqdm(image_pcd_pairs):
+            # print(f"Processing {image_path} and {pcd_path} with diff={diff:.3f} sec")
+            # Load pointclouds
+            # pcd = o3d.io.read_point_cloud(pcd_path.as_posix())
+            pcd = get_accumulated_pcd(
+                pcd_path,
+                T_WBs,
+                accumulation_length=accum_length,
+                max_time_diff_camera_and_pose=max_time_diff_camera_and_pose,
+            )
+            if pcd is None:
+                if accum_length == 0:
+                    print(f"{pcd_path} pose not found")
+                    continue
+                else:
+                    raise RuntimeError(f"{pcd_path} pose not found")
+            # check if point cloud is empty
+            if len(pcd.points) == 0:
+                print(f"{pcd_path} is empty")
+                continue
+            # Transform points into camera coordinates
+            pcd.transform(T_cam_base)  # transform lidar points from base to camera frame
+            depth, normal = get_depth_from_cloud(pcd, K, D, w, h, fov_deg, camera_model, depth_factor, is_euclidean)
+            save_projection_outputs(
+                depth,
+                normal,
+                image_path,
+                save_depth_path=(target_depth_subdir / (image_path.stem + ".png")).as_posix(),
+                save_normal_path=(target_normal_subdir / (image_path.stem + ".png")).as_posix(),
+                save_overlay_path=(target_overlay_subdir / (image_path.stem + ".jpg")).as_posix(),
+            )
+
+
+def get_transforms(
+    robotics_pose_file,
+    depth_pose_format,
+    scale_factor,
+    T_BC,
+    camera_topic,
+    image_folder_name,
+    frame="camera",  # returned pose's frame. base: T_WB; camera: T_WC
+    visualise=False,
+):
+    # Read trajectory
+    if depth_pose_format == "vilens_slam":
+        reader = VilensSlamTrajReader(robotics_pose_file)
+        pose_traj = reader.read_file()
+        pose_traj.scale(scale_factor)  # T_WB
+        # pose_traj.transform(PoseConvention.get_transform("robotics", "vision"), right_mul=True)
+        if frame == "camera":
+            pose_traj.transform(T_BC, right_mul=True)  # T_BC * T_WB=T_WC
+        # raise NotImplementedError("TODO: fix this")
+    elif depth_pose_format == "nerf":
+        camera_folder = image_folder_name + "/" + camera_topic
+        reader = NeRFTrajReader(robotics_pose_file, camera_folder, nerf_reader_sort_timestamp=True)
+        pose_traj = reader.read_file()
+        pose_traj.scale(scale_factor)  # T_WC in nerf convention
+        pose_traj.transform(PoseConvention.get_transform("nerf", "vision"), right_mul=True)
+        if frame == "base":
+            T_CB = np.linalg.inv(T_BC)
+            pose_traj.transform(T_CB, right_mul=True)  # T_WB
+
+    pcds = [o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.01)]
+    scaled_Ts = {}
+    for key, T in zip(pose_traj.timestamps, pose_traj.poses_se3):
+        axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5)
+        axis.transform(T)
+        pcds.append(axis)
+        scaled_Ts[str(key)] = T
+    if visualise:
+        print("Visualize loaded pose.")
+        print("Press ECS to exit.")
+        o3d.visualization.draw_geometries(pcds)
+    return scaled_Ts
